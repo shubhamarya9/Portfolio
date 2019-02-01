cbf76f7897f30f971e9a4c4528f37314301b598d
shubham arya <shubhamarya@shubhams-MacBook-Pro.local>
2019-02-01 01:35:01 -0500

    Updates

---
+++ precache-manifest.b798c7d884b04c47a3b6dfddca44fde1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dd4ba5f36c48d7453dd",
+    "url": "/Portfolio/static/css/main.0998fdcc.chunk.css"
+  },
+  {
+    "revision": "5dd4ba5f36c48d7453dd",
+    "url": "/Portfolio/static/js/main.5dd4ba5f.chunk.js"
+  },
+  {
+    "revision": "cdd746e31490a56daee8",
+    "url": "/Portfolio/static/css/1.2041db5e.chunk.css"
+  },
+  {
+    "revision": "cdd746e31490a56daee8",
+    "url": "/Portfolio/static/js/1.cdd746e3.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "f62c70387ae7c6f675b8a2fba26c8c8a",
+    "url": "/Portfolio/static/media/avatar.f62c7038.png"
+  },
+  {
+    "revision": "7013a80015a93b61d6406ffd0bc2588e",
+    "url": "/Portfolio/index.html"
+  }
+];
