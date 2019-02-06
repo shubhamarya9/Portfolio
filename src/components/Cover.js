946ab5c5b56dc4d037e1c7494b7c219927427407
shubham arya <shubhamarya@shubhams-MacBook-Pro.local>
2019-02-06 01:19:15 -0500

    css changes

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -10,15 +10,14 @@ class Cover extends Component {
     return (
       <Router>
         <div
+          className="landing-grid"
           style={{
-            top: 0,
-            left: 0,
             width: "100%",
             height: "100%"
           }}
         >
-          <Grid className="landing-grid">
-            <ParticleComponent />
+          <ParticleComponent />
+          <Grid>
             <Cell col={12}>
               <img src={img} className="avatar-img" alt="animoji" />
               <div className="type-writer">
@@ -40,42 +39,45 @@ class Cover extends Component {
                 <p>
                   HTML/CSS | JavaScript | React | NodeJS | Express | MongoDB
                 </p>
+                <div className="social-links">
+                  <h3 style={{ color: "white" }}>
+                    My Social Links: <br />
+                  </h3>
+
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: "none" }}
+                    href="https://www.linkedin.com/in/shubham-arya-630957163/"
+                  >
+                    <i
+                      className="fab fa-linkedin linkd"
+                      aria-hidden="true"
+                      id="linkedin"
+                    />
+                  </a>
+                  <a
+                    href="https://www.instagram.com/__shubhamaaryaa.js/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i
+                      className="fab fa-instagram"
+                      aria-hidden="true"
+                      id="insta"
+                    />
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: "none" }}
+                    href="https://github.com/shubhamarya9/"
+                  >
+                    <i className="fab fa-github" aria-hidden="true" id="git" />
+                  </a>
+                </div>
               </div>
             </Cell>
-
-            <div className="social-links">
-              <h3>
-                My Social Links: <br />
-              </h3>
-
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ textDecoration: "none" }}
-                href="https://www.linkedin.com/in/shubham-arya-630957163/"
-              >
-                <i
-                  className="fab fa-linkedin linkd"
-                  aria-hidden="true"
-                  id="linkedin"
-                />
-              </a>
-              <a
-                href="https://www.instagram.com/__shubhamaaryaa.js/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-instagram" aria-hidden="true" id="insta" />
-              </a>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ textDecoration: "none" }}
-                href="https://github.com/shubhamarya9/"
-              >
-                <i className="fab fa-github" aria-hidden="true" id="git" />
-              </a>
-            </div>
           </Grid>
         </div>
       </Router>
